356fe56a1bb02d045fc57f12f3b37657761dcb3b
ethan <wizardcat@pm.me>
2019-07-31 11:37:11 +1000

    included map generation feature
    map generator for rpg game

---
+++ rpgmapgenerator/main.py
@@ -0,0 +1,26 @@
+
+with open("map.txt") as f:
+	map = []
+	lines = f.read().split("\n")
+	for i in range(len(lines[0])):
+		map.append([])
+	for y in range(len(lines)):
+		for x in range(len(lines[0])):
+			tile = lines[x][y]
+			square = str()
+			if tile == "o":
+		 		square += '{type: \'ocean\', canStep: false}'
+		 	elif tile == "d":
+		 		square += '{type: \'dirt\'}'
+		 	map[x].append(square)
+	print(str(map).replace('"', '').replace("],","],\n"))
+
+
+# print len(lines)
+# fname = input("Enter file name: ")
+# num_lines = 0
+# with open(fname, 'r') as f:
+#     for line in f:
+#         num_lines += 1
+# print("Number of lines:")
+# print(num_lines)
