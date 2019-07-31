97722c12822c294d15da89136e2548249e059750
henry <henry.confos@student.tsc.nsw.edu.au>
2019-07-31 21:00:35 +1000

    stats

--- inventory.js
+++ inventory.js
@@ -8,8 +8,14 @@ function Inventory(){
 		var slotNum = this.items.length;
 		if (slotNum < 9) {
 			$( ".slot" ).eq( slotNum-1 ).addClass(item.class)
+			$( ".slot" ).eq( slotNum-1 ).attr('title', item.name);
 		}else{
 			$( ".slot" ).eq( 8 ).addClass(item.class)
 		}
+	}
+
+	this.use = function(id){
+		console.log(this.items[0]);
+		player.updateStat(this.items[id].attribute,true,this.items[id].byAmount)
 	}
 }
--- player.js
+++ player.js
@@ -1,7 +1,6 @@
 function Player(x,y) {
   this.level = 1;
-  this.health = 100;
-  this.score = 0;
+  this.attr = [10,50,50,50,50];
   this.cX = 0;
   this.cY = 0;
   this.x = 0;
@@ -17,6 +16,18 @@ function Player(x,y) {
   };
 
   this.drawStats = function() {
-    
+    for (var i = 0; i < this.attr.length; i++) {
+      $( "li" ).eq( i ).find( ".prog-bar-inner" ).css("width", this.attr[i]+'%');
+      $( "li" ).eq( i ).find( ".prog-bar-inner" ).text(this.attr[i]);
+    }
+  }
+
+  this.updateStat = function(id,pos,amount){
+    if (pos) {
+      this.attr[id] = this.attr[id] + amount;
+    }else{
+      this.attr[id] = this.attr[id] - amount;
+    }
+    this.drawStats();
   }
 }
