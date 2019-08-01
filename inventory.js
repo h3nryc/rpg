8f3955ae2d98eb90859439eb753c4f679c75f69d
henry <henry.confos@student.tsc.nsw.edu.au>
2019-08-01 12:18:18 +1000

    iodfghkujlx

--- inventory.js
+++ inventory.js
@@ -14,14 +14,20 @@ function Inventory(){
 		}
 	}
 
+	this.emptyItems = function(){
+		for (var id = 0; id < this.items.length; id++) {
+			$( ".slot" ).eq( id ).removeClass(this.items[id].class)
+			$( ".slot" ).eq( id ).attr('title', "");
+		}
+		this.drawItems();
+	}
+
 	this.drawItems = function(){
 		for (var i = 0; i < this.items.length; i++) {
-			if (i < 9) {
-				$( ".slot" ).eq( i-1 ).addClass(this.items[i].class)
-				$( ".slot" ).eq( i-1 ).attr('title', this.items[i].name);
-			}else{
-				$( ".slot" ).eq( 8 ).addClass(this.items.class)
-			}
+				console.log(i);
+				console.log($( ".slot" ));
+				$( ".slot" ).eq( i ).addClass(this.items[i].class)
+				$( ".slot" ).eq( i ).attr('title', this.items[i].name);
 		}
 	}
 
@@ -30,10 +36,12 @@ function Inventory(){
 		$( ".slot" ).eq( id ).removeClass(this.items[id].class)
 		$( ".slot" ).eq( id ).attr('title', "");
 		this.items.splice(id, 1);
+		this.emptyItems();
 	}
 
 	this.use = function(id){
 		console.log(this.items[0]);
 		player.updateStat(this.items[id].attribute,true,this.items[id].byAmount)
+		this.remove(id)
 	}
 }
